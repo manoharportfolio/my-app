be8d36cff2c360242ba312018358ac8da5da1156
manoharportfolio <gmanohar739@gmail.com>
2025-04-19 21:13:26 +0530

    video 12

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import React, {useState} from 'react';
+
 
 function App() {
+  const [mode,setMode] = useState('light');
+  const toggleMode=()=>{
+    if(mode === 'light'){
+      setMode('dark');
+    }else{
+      setMode('light');
+    }
+  }
+
   return (
     <>
-      <Navbar title="Textutils" />
+      <Navbar title="Textutils" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-5">
         <TextForm heading="Enter your text to analyze below"/>
       </div>

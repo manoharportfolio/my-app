8501e6b33c34be255500c4bf5d0d2089f6070dce
manoharportfolio <gmanohar739@gmail.com>
2025-04-19 19:37:08 +0530

    video 11

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,6 +8,10 @@ export default function TextForm(props) {
     let newText =text.toUpperCase();
     setText(newText);
   }
+  const handleClear =()=>{
+    let newText ='';
+    setText(newText);
+  }
   const handleLowClick =()=>{
 
     let newText =text.toLowerCase();
@@ -16,6 +20,15 @@ export default function TextForm(props) {
   const handleOnChange=(event)=>{
     setText(event.target.value);
     
+  }
+  const handleCopy =()=>{
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+  const handleExtraSpaces =()=>{
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
   }
   const [text,setText] =useState('');
   return (<>
@@ -32,6 +45,9 @@ export default function TextForm(props) {
       </div>
       <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert Uppercase</button>
       <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert Lowercase</button>
+      <button className="btn btn-primary mx-1" onClick={handleClear}>Clear Text</button>
+      <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy</button>
+      <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Extar Space Remove</button>
     </div>
     <div className="container">
       <h2>Your text summary</h2>
